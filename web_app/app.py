2d49ef1f5463f1e8f860889fd5f34b5b73ba34fe
gulya99 <78430078+gulya99@users.noreply.github.com>
2024-05-20 11:54:12 -0400

    update index.htm web app

--- web_app/app.py
+++ web_app/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileRequired
 from wtforms import FileField, SubmitField
+from wtforms.validators import InputRequired
+from werkzeug.utils import secure_filename
 import os
 import requests
 
@@ -10,28 +11,23 @@ app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/files'
 
 class UploadFileForm(FlaskForm):
-    file = FileField("File", validators=[
-      FileRequired("File field shoud not be empty")
-    ])
-    submit = SubmitField("Upload")
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
 
 @app.route("/health")
 def health():
     return "OK"
 
-@app.route('/')
-def root():
-    return "Hello world!"
 
-@app.route('/home', methods=['GET',"POST"])
+@app.route('/', methods=['GET',"POST"])
 def home():
     form = UploadFileForm()
     if form.validate_on_submit():
-        file = form.file.data # First grab the file
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename)) # Then save the file
+        filename = secure_filename(form.file.data.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         image = {"image": file.filename}
         response = requests.post('http://0.0.0.0:6000/detect', files=files)
-    return render_template('index.html', form=form)
+    return render_template('index.html', form=form, filename=filename)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
