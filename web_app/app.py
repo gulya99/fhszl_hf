3309f17b1ec7095d08d2a61372e1ba1986d74b3c
Gulyás Gergely <78430078+gulya99@users.noreply.github.com>
2024-05-20 18:06:31 -0400

    mukodik?

--- web_app/app.py
+++ web_app/app.py
@@ -5,26 +5,26 @@ import time
 import requests
 import threading
 
-web_app = Flask(__name__)
-web_app.config["UPLOAD_FOLDER"] = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images")
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = "./images"
 
 def detect():
     car_cascade = cv2.CascadeClassifier("car_haarcascade.xml")
 
-@web_app.route("/health")
+@app.route("/health")
 def health():
     return "OK"
 
-@web_app.route("/")
+@app.route("/")
 def home():
     return render_template("index.html")
 
-@web_app.route('/upload', methods=["GET","POST"])
+@app.route('/upload', methods=["GET","POST"])
 def upload():
     tag = request.form.get("tag")
     image = request.files.get("image")
-    image.save(os.path.join(web_app.config["UPLOAD_FOLDER"], image.filename))
+    image.save(os.path.join(app.config["UPLOAD_FOLDER"], image.filename))
     return render_template("image.html", title=tag, image=image.filename)
 
 if __name__ == '__main__':
-    web_app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
