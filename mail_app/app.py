df29bda6c8a7a61049160e950f1510cbfc7daa19
Gulyás Gergely <78430078+gulya99@users.noreply.github.com>
2024-05-21 00:16:46 -0400

    email?

--- mail_app/app.py
+++ mail_app/app.py
@@ -3,12 +3,13 @@ from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_SERVER'] = 'live.smtp.mailtrap.io'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'gulyi.geri@gmail.com'  # Use your actual Gmail address
-app.config['MAIL_PASSWORD'] = 'Nagysztgergely'     # Use your generated App Password
+app.config['MAIL_USERNAME'] = 'api'  # Use your actual Gmail address
+app.config['MAIL_PASSWORD'] = 'c66fd364d82023c0747ad61ee7c4920d'     # Use your generated App Password
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
+
 mail = Mail(app)
 
 @app.route("/health")
@@ -19,8 +20,8 @@ def health():
 def index():
     msg = Message(
         subject='Hello from the other side!', 
-        sender='gulyi.geri@gmail.com',  # Ensure this matches MAIL_USERNAME
-        recipients=['gugerobo@gmail.com']  # Replace with actual recipient's email
+        sender='eh9jtc@mailtrap.club',  # Ensure this matches MAIL_USERNAME
+        recipients=['gulyi.geri@gmail.com']  # Replace with actual recipient's email
     )
     msg.body = "Hey, sending you this email from my Flask app, let me know if it works."
     mail.send(msg)
