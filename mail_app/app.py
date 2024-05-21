598e1b0e52d2332ccebe3c1debf7d4dca1b7392b
Gulyás Gergely <78430078+gulya99@users.noreply.github.com>
2024-05-20 20:46:25 -0400

    email update

--- mail_app/app.py
+++ mail_app/app.py
@@ -3,11 +3,11 @@ from flask_mail import Mail, Message
 import redis
 
 app = Flask(__name__)
-app.config['MAIL_SERVER'] = 'smtp.example.com'
+app.config['MAIL_SERVER'] = 'smtp.fhszlhf.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'your-email@example.com'
-app.config['MAIL_PASSWORD'] = 'your-email-password'
+app.config['MAIL_USERNAME'] = 'eh9jtc@fhszlhf.com'
+app.config['MAIL_PASSWORD'] = 'secretpassword'
 
 mail = Mail(app)
 
