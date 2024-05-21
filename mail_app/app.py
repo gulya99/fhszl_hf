c85f7e872dfe297e8bbb577bd7bdf0c886469a73
Gulyás Gergely <78430078+gulya99@users.noreply.github.com>
2024-05-21 00:50:00 -0400

    mail

--- mail_app/app.py
+++ mail_app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config['MAIL_SERVER']='bulk.smtp.mailtrap.io'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'api'
+app.config['MAIL_USERNAME'] = 'gulyi.geri@gmail.com'
 app.config['MAIL_PASSWORD'] = 'c66fd364d82023c0747ad61ee7c4920d'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
@@ -16,12 +16,12 @@ mail = Mail(app)
 def health():
     return "OK"
 
-@app.route("/send")
+@app.route("/")
 def index():
     msg = Message(
         subject='Hello from the other side!', 
-        sender='gulyi.geri@gmail.com',
-        recipients=['gugerobo@gmail.com']
+        sender='peter@mailtrap.io',
+        recipients=['gulyi.geri@gmail.com']
     )
     msg.body = "Hey, sending you this email from my Flask app, let me know if it works."
     mail.send(msg)
